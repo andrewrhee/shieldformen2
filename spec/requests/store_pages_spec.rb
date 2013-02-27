c60f7c1711e7a65a256751ad346d877f29c6b841
Andrew Rhee <adrhee@gmail.com>
2013-02-27 15:11:48 -0800

    Create store layout pages

--- spec/requests/store_pages_spec.rb
+++ spec/requests/store_pages_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe "Store pages" do
+	subject { page }
 
+	describe "index page" do
+  	before { visit store_path }
+
+  	it { should have_selector('h1', text: "Store") }
+  	it { should have_selector('title', text: "Store") }
+  end
 end
